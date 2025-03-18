24754558ccda10da82e6eb8b330d6d0c9dc65325
Marek Zelinka <mzelinka17@gmail.com>
2025-03-18 14:46:00 +0100

    fix logo

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-chart-1/10 p-6 md:p-10">
       <div className="flex w-full max-w-[400px] flex-col gap-6">
         <div className="flex items-center gap-2 self-center font-medium">
-          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-chart-1/70 text-background">
             <BarChartBigIcon aria-hidden className="size-4" />
           </div>
           Expenses Chart
