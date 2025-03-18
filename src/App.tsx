007443e17b29d4aac76305554eab548ed76e8852
Marek Zelinka <mzelinka17@gmail.com>
2025-03-18 11:31:20 +0100

    missing aria-hidden

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
       <div className="flex w-full max-w-[400px] flex-col gap-6">
         <div className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <BarChartBigIcon className="size-4" />
+            <BarChartBigIcon aria-hidden className="size-4" />
           </div>
           Expenses Chart
         </div>
